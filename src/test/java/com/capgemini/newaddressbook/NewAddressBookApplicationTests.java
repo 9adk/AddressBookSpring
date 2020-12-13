b480035cfa7c7bf348bef13c46667b576e9caefb
aditya Kharade <adityakharade51@gmail.com>
2020-12-13 21:17:03 +0530

    Welcome to addressbook app

---
+++ src/test/java/com/capgemini/newaddressbook/NewAddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.newaddressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
